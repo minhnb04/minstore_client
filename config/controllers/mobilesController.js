2aa7ba619437552ca9c584edb51d093e12eda7cb
minhnb04 <minhnb04@gmail.com>
2023-07-14 12:45:53 +0700

    Hoàn thành chức năng sắp xếp Mobiles list

--- config/controllers/mobilesController.js
+++ config/controllers/mobilesController.js
@@ -15,6 +15,34 @@ class MobilesController{
             });
     }
 
+    soft(req, res, next) {
+        axios.get('https://minstore-admin.onrender.com/products/getProducts')
+            .then(response => {
+                // Nhận dữ liệu JSON từ phản hồi
+                const data = response.data;
+
+                if (req.params.softmethod == 'outstanding') {
+
+                    res.render('mobiles', {title: 'Mobiles', data: data})
+                } else if (req.params.softmethod == 'pricehightolow') {
+                    data.sort((a, b) => b.price - a.price);
+                    res.render('mobiles', {title: 'Mobiles', data: data})
+                } else if (req.params.softmethod == 'pricelowtohigh') {
+                    data.sort((a, b) => a.price - b.price);
+                    res.render('mobiles', {title: 'Mobiles', data: data})
+                } else if (req.params.softmethod == 'alphabet') {
+                    data.sort((a, b) => a.productName.localeCompare(b.productName));
+                    res.render('mobiles', {title: 'Mobiles', data: data})
+                } else {
+                    res.json('softmethod sai!!!')
+                }
+
+            })
+            .catch(error => {
+                res.render('error', {title: 'Error', error: error})
+            });
+
+    }
 }
 
 module.exports = new MobilesController;
--- routes/mobiles.js
+++ routes/mobiles.js
@@ -6,4 +6,8 @@ const mobilesController = require('../config/controllers/mobilesController')
 
 router.get('/', mobilesController.index)
 
+router.get('/soft/:softmethod', mobilesController.soft)
+
+
+
 module.exports = router;
