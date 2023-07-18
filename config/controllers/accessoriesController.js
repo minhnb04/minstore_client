8621a5b3991b2038d1cf20e5111f13e1bbd871e1
minhnb04 <minhnb04@gmail.com>
2023-07-18 15:09:44 +0700

    Call api login và xác thực bằng jwt

--- config/controllers/accessoriesController.js
+++ config/controllers/accessoriesController.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 class AccessoriesController{
 
     index(req, res){
-        axios.get('http://localhost:8080/productCilent/getAccessories')
+        axios.get('https://minstore-admin.onrender.com/productCilent/getAccessories')
             .then(response => {
                 const data = response.data;
                 res.render('accessories',{ title: 'Accessories', data: data , userlogin: res.userlogin })
